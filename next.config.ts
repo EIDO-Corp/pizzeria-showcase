cdbecee46313bf93e81068abc3904b9b95db4820
IDRAYNAR <rayan.idri@edu.devinci.fr>
2025-03-17 18:28:12 +0100

    fix(config): update config to host on vercel

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // For GitHub Pages with repository name in the URL path
-  basePath: '/pizzeria-showcase',
   images: {
     unoptimized: true, // Required for static export
   },
